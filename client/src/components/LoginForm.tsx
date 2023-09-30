5c12d9d07b2abc03013c721cc528593f3d59c723
Aaron <aaronlmjohnson307@gmail.com>
2023-09-30 16:05:40 -0400

    added userLogout hook

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,38 +1,16 @@
 import FormInput from "./FormInput";
+import { useLogin } from "../hooks/useLogin";
 import { useState } from "react";
 
 const LoginForm = ()=>{
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
+    const {login, isLoading, error} = useLogin();
 
     const handleSubmit =  async (e:any)=>{
         e.preventDefault();
-        
-        const formContent =  { username, password, role: "Developer" }
-        const response = await fetch("http://localhost:3001/login", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formContent),
-        });
-        console.log(response)
-
-        //const json = await response.json();
-
-
-        if(!response.ok){
-            console.log('error')
-            // setError(json.error);
-        } 
-        if(response.ok){
-            console.log(`User ${username} has logged in.`)
-            setError(null);
-            setUsername('');
-            setPassword('');
-        }
+        await login(username, password);
     }
 
     return (
---
+++ client/src/hooks/useLogin.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = ()=>{
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
+
+    const login = async(username:String, password:string)=>{
+        setIsLoading(true);
+        setError("");
+        const response = await fetch("http://localhost:3001/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password}),
+        })
+
+        const json = await response.json();
+        if(!response.ok){
+            setIsLoading(false);
+            console.log(json);
+            setError(json.error);
+        }
+
+        if(response.ok){
+            setIsLoading(false);
+            setError("")
+            localStorage.setItem('user', JSON.stringify(json));
+            console.log(`Welcome to Issue Tracker ${username}.`);
+            dispatch({type: 'LOGIN', payload: json});
+            navigate(json.redirectUrl);
+
+        }
+    }
+    return {
+        login,
+        isLoading,
+        error
+    }
+}
+
