2c845b7c4ff5fb16e39790e31dfdfa6be96ed79c
Aaron <aaronlmjohnson307@gmail.com>
2023-10-15 15:42:50 -0400

    added update button to project listings

--- client/src/components/ProjectListing.tsx
+++ client/src/components/ProjectListing.tsx
@@ -1,13 +1,23 @@
 import ProjectDeleteButton from "../hooks/ProjectDeleteButton";
+import useProjectUpdateButton from "./useProjectUpdateButton";
+import { useEffect } from "react";
 
 const ProjectListing = (props:any)=>{
     const { project } = props;
+    const { updateButton, cancelButton, formActive, setFormActive} = useProjectUpdateButton();
+
+    useEffect(()=>{
+        props.setFormActive((prevState:boolean)=> formActive);
+        props.setActiveProject((prevState:any)=> project);
+    }, [formActive]);
     return(
         <div className="project-listing">
             <h1><a href={`/projects/${project._id}`}>{project.title}</a></h1>
             <p>{project.date_created}</p>
             <p>{project.description}</p>
+            {formActive ? cancelButton() : updateButton()}
             <ProjectDeleteButton project = {project}/>
+
         </div>
     )
 }
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState} from 'react';
 import ProjectListing from '../components/ProjectListing';
+import CreateProjectForm from './CreateProjectForm';
 
 interface Project {
     _id: string;
@@ -10,6 +11,9 @@ interface Project {
 
 const Projects = ()=>{
     const [projects, setProjects] = useState<Array<Project>>([]);
+    const [activeProject, setActiveProject] = useState(null);
+    const [formActive, setFormActive] = useState(null);
+
     useEffect(()=>{
         const fetchProjects = async ()=> {
             const response = await fetch('http://localhost:3001/projects');
@@ -26,8 +30,16 @@ const Projects = ()=>{
     return(
         <div className="projects">
                 {projects && projects.map((project)=>{
-                    return(<ProjectListing  project={project} key={project._id}/>)
+                    return(
+                        <ProjectListing  
+                                project={project} 
+                                key={project._id} 
+                                setFormActive = {setFormActive}
+                                setActiveProject = {setActiveProject}
+                        />)
                 })}
+
+            {formActive && <CreateProjectForm update={true} project={activeProject} setFormActive={setFormActive} formActive={formActive}/>}
         </div>
     );
 }
