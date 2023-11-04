576e7a77eb7d70a35e4cf1d2e3d8fa98082aa814
Aaron <aaronlmjohnson307@gmail.com>
2023-11-04 17:41:53 -0400

    logging out redirects the user to login page

--- client/src/hooks/useLogout.tsx
+++ client/src/hooks/useLogout.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
-import { useNavigate } from 'react-router-dom';
+import { redirect, useNavigate } from 'react-router-dom';
 
 export const useLogout = ()=>{
 
     const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
 
     const logout = ()=>{
         localStorage.removeItem('user');
         console.log(`${localStorage.getItem('user')} has logged out.`);
         dispatch({type:"LOGOUT"});
+        navigate('/login');
     }
     
     return {
