d2a213f9761c7afcb11bbe9e3caa21bb6cc3a9a9
Aaron <aaronlmjohnson307@gmail.com>
2023-11-04 16:52:31 -0400

    added user routes to browser router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Root from "./routes/Root";
 import { AuthContextProvider } from './context/AuthContext';
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import ErrorPage from './pages/errorPages/ErrorPage';
+import SignUp from './pages/SignUp';
+import GuestLoginPage from './pages/GuestLoginPage';
+import UsersPage from './pages/UsersPage';
+import UserDetail from './components/UserDetail';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,26 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />
   },
+  {
+    path: "/sign-up",
+    element: <SignUp page={"signup"}/>,
+  },
+  {
+    path: "/login",
+    element: <SignUp page={"login"}/>,
+  },
+  {
+    path: "/guest-login",
+    element: <GuestLoginPage />,
+  },
+  {
+    path:"/users",
+    element: <UsersPage />
+  },
+  {
+    path:"/users/:userId",
+    element: <UserDetail />
+  }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
