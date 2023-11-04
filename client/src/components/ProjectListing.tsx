ace123b17dccb2ff774753e65c2d4f2e319b8d63
Aaron <aaronlmjohnson307@gmail.com>
2023-11-04 15:49:05 -0400

    admins and project leads can now only edit and delete projects

--- client/src/components/ProjectListing.tsx
+++ client/src/components/ProjectListing.tsx
@@ -1,4 +1,5 @@
 import ProjectDeleteButton from "../hooks/ProjectDeleteButton";
+import useCheckAuthorization from "../hooks/useCheckAuthorization";
 import useDeleteConfirmation from "../hooks/useDeleteConfirmation";
 import useProjectUpdateButton from "../hooks/useProjectUpdateButton";
 import { useEffect } from "react";
@@ -8,8 +9,10 @@ const ProjectListing = (props:any)=>{
     const { updateButton, cancelButton, formActive, setFormActive} = useProjectUpdateButton();
     const url = `http://localhost:3001/projects/${project._id}/delete`;
     const { display:showDeleteConfirmation, setDisplay, confirmationForm} = useDeleteConfirmation(url, project);
+    const {isAuthed, isAuthedToEditProject} = useCheckAuthorization();
 
     useEffect(()=>{
+        isAuthedToEditProject(project);
         props.setFormActive((prevState:boolean)=> formActive);
         props.setActiveProject((prevState:any)=> project);
         props.setToggleUpdate((prevState: any)=> prevState ? false : true);
@@ -17,13 +20,21 @@ const ProjectListing = (props:any)=>{
         //if toggleUpdate true set toggle create to false
     }, [formActive]);
 
+    const alterationButtons = ()=>{
+        return (
+            <>
+                {updateButton()}
+                <ProjectDeleteButton project = {project} setDisplay={setDisplay}/>
+            </>
+        )
+    }
+
     return(
         <div className="project-listing">
             <h1><a href={`/projects/${project._id}`}>{project.title}</a></h1>
             <p>{project.date_created}</p>
             <p>{project.description}</p>
-            {updateButton()}
-            <ProjectDeleteButton project = {project} setDisplay={setDisplay}/>
+            {isAuthed && alterationButtons()}
             {showDeleteConfirmation && confirmationForm()}
         </div>
     )
