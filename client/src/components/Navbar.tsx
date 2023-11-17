464e4e776a69f42ba9d493a0e57202231047611b
Aaron <aaronlmjohnson307@gmail.com>
2023-11-17 12:49:14 -0500

    removed unnecessary comments

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -47,28 +47,3 @@ const Navbar = ()=>{
 }   
 
 export default Navbar;
-
-
-// import { useNavigate } from "react-router-dom";
-// import { useAuthContext } from "../hooks/useAuthContext";
-// import { useLogout } from "../hooks/useLogout";
-
-// const Navbar = ()=>{
-//     const { user } = useAuthContext();
-//     const { logout } = useLogout();
-    
-//     return(
-//         <nav className="border-b-4 border-t-0 border-x-0  border-black">
-//             <p className=""><a href="/dashboard">Home</a></p>
-//             <p className=""><a href="/projects">Projects</a></p>
-//             <p className=""><a href="/users">Users</a></p>
-//             <p className=""><a href="/tickets">Tickets</a></p>
-//             {user ? <p>Logged in As: {user.user.fullName}</p> : <></>}
-//             {user && <button onClick={logout}>Log out</button>}
-//             {!user && <p><a href="/login">Sign in</a></p>}
-//             {!user && <p><a href="/sign-up">Sign Up</a></p>}
-//         </nav>
-//     )
-// }
-
-// export default Navbar;
