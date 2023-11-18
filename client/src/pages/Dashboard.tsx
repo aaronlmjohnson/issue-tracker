e807778100509b0e3d5d9513340fb2ff718aca8e
Aaron <aaronlmjohnson307@gmail.com>
2023-11-18 15:47:57 -0500

    added DashboardHeader

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,16 +4,21 @@ import { faAngleDown} from '@fortawesome/free-solid-svg-icons';
 const Dashboard = ()=>{
     const arrowDown = <FontAwesomeIcon icon={faAngleDown} />
 
-    return (
-        <div className="dashboard ">
-            <div>
-                <h1>Recent Activity</h1>
-                <button>
+    const DashboardHeader = ()=>{
+        return(
+            <div className="flex justify-between">
+                <h1 className="font-primary text-5xl font-extrabold">Recent Activity</h1>
+                <button className="flex font-secondary items-center text-non-focus gap-2.5">
                     view all
                     {arrowDown}
                 </button>
             </div>
-            
+        )
+    }
+
+    return (
+        <div className="dashboard p-7">
+            <DashboardHeader />
         </div>
     );
 }
