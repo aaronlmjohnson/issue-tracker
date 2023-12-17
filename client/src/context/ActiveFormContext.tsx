3da0cd602a288ced06ef7ada7a82976f5b4195ec
Aaron <aaronlmjohnson307@gmail.com>
2023-12-17 15:36:36 -0500

    added updateTarget to ActiveFormContext

--- client/src/context/ActiveFormContext.tsx
+++ client/src/context/ActiveFormContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState} from 'react';
+import {TProject, TTicket} from '../lib/types';
 
 type TActiveForm = "create-project" | 
                    "create-ticket" | 
@@ -6,12 +7,11 @@ type TActiveForm = "create-project" |
                    "update-ticket" |
                    "none";
 
-
 interface IActiveFormContext {
     activeForm: TActiveForm | null,
     setActiveForm: React.Dispatch<React.SetStateAction<TActiveForm>>
-    updateTarget: null,
-    setUpdateTarget: React.Dispatch<React.SetStateAction<null>>
+    updateTarget: TProject | TTicket | null
+    setUpdateTarget: React.Dispatch<React.SetStateAction<TProject | TTicket | null>>
 }
 
 export const ActiveFormContext = createContext<IActiveFormContext | null>(null);
@@ -22,7 +22,7 @@ interface IActiveFormProviderProps{
 
 export const ActiveFormContextProvider = ({children}:IActiveFormProviderProps)=>{
     const [activeForm, setActiveForm] = useState<TActiveForm>("none");
-    const [updateTarget, setUpdateTarget] = useState(null);
+    const [updateTarget, setUpdateTarget] = useState<TProject | TTicket | null>(null);
 
     return(
         <ActiveFormContext.Provider value={{activeForm, setActiveForm, updateTarget, setUpdateTarget}}>
