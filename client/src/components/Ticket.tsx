239c2029dd3ba0cb0452e149fc2659b653ee41ba
Aaron <aaronlmjohnson307@gmail.com>
2024-01-16 11:56:13 -0500

    added priority colors to tailwind config

--- client/src/components/Ticket.tsx
+++ client/src/components/Ticket.tsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useFetchData } from '../hooks/useFetchData';
-import TicketDetail from './TicketDetail';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useActiveFormContext } from '../hooks/useActiveFormContext';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Ticket = (props:any)=>{
     const { ticket, setActiveTicket } = props;
     const {activeDetail, setActiveDetail} = useActiveFormContext();
 
     const {data:author, loading:authorLoading} = useFetchData(`/users/${ticket.author._id}`);
-    const [toggleDetail, setToggleDetail] = useState(false);
     const navigate = useNavigate();
     const params = useParams();
 
@@ -22,14 +22,15 @@ const Ticket = (props:any)=>{
     }
     
     return (
-        !authorLoading && <div className="ticket">
-            <button onClick={handleTicketDetail}>{ticket.title}</button>
-            <p>{author.username}</p>
-            <p>{ticket.feature}</p>
-            <p>{ticket.priority}</p>
-            <p>{ticket.status}</p>
-            {/* <Outlet context={ticket}/> */}
-        </div>
+        !authorLoading && <button onClick={handleTicketDetail}>
+            <ul className="ticket-listing">
+                <li><p>{ticket.priority}</p></li>
+                <li><h2>{ticket.title}</h2></li>
+                <li><p>{ticket.feature}</p></li>
+                <li><p>{ticket.date_created}</p></li>
+                <li><p>{ticket.status}</p></li>
+            </ul>
+        </button>
     )
 }
 
