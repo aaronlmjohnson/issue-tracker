514ba00e5abf43ded17bab8cee7378843c01db7a
Aaron <aaronlmjohnson307@gmail.com>
2024-01-28 23:40:11 -0500

    added content to api index page

--- server/app.ts
+++ server/app.ts
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //API Index Page
 app.get('/', function (req, res) {
-  res.send({ title: 'Issue Tracker' });
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.use(activityRouter);
